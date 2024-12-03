a713913fd0eefdca31d5c00b1859845fb363dfa1
EmillySatiro <sousasatiroclaraemilly@gmail.com>
2024-12-02 22:16:42 -0300

    att e pronto

--- scripts/compilar_e_rodar.sh
+++ scripts/compilar_e_rodar.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 
-# Se o caminho do projeto não for passado como argumento, assume o diretório atual
-PROJECT_DIR="${1:-$(dirname "$(realpath "$0")")}"
+# Diretório base do projeto
+BASE_DIR="/home/Trabalho-2-ed-II"
 
 # Nome do executável
 EXECUTAVEL="programa"
 
 # Arquivos necessários
-MAIN_FILE="$PROJECT_DIR/main.c"
-BINARIA_FILE="$PROJECT_DIR/Arvores/Binaria/ARV_BINARIA.c"
-ARVORE_2_3_FILE="$PROJECT_DIR/Arvores/Arvore_2_3/ARV2_3.c"
-RUBRO_NEGRA_FILE="$PROJECT_DIR/Arvores/Rubro_negra/Rubro_negra.c"
-
-# Verificar se todos os arquivos existem
-if [ ! -f "$MAIN_FILE" ] || [ ! -f "$BINARIA_FILE" ] || [ ! -f "$ARVORE_2_3_FILE" ] || [ ! -f "$RUBRO_NEGRA_FILE" ]; then
-    echo "Erro: Alguns arquivos necessários não foram encontrados no diretório do projeto."
-    exit 1
-fi
+MAIN_FILE="$BASE_DIR/main.c"
+BINARIA_FILE="$BASE_DIR/Arvores/Binaria/ARV_BINARIA.c"
+ARVORE_2_3_FILE="$BASE_DIR/Arvores/Arvore_2_3/ARV2_3.c"
+RUBRO_NEGRA_FILE="$BASE_DIR/Arvores/Rubro_negra/Rubro_negra.c"
 
 # Compilar o programa
 echo "Compilando o programa..."
-
 gcc -o "$EXECUTAVEL" \
--I"$PROJECT_DIR" \
+-I"$BASE_DIR" \
 "$MAIN_FILE" \
 "$BINARIA_FILE" \
 "$ARVORE_2_3_FILE" \
