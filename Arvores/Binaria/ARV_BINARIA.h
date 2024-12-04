ebcf48b968a627f0955cf14fb2c67417d9aec745
EmillySatiro <sousasatiroclaraemilly@gmail.com>
2024-12-03 22:08:41 -0300

    att binaria

--- Arvores/Binaria/ARV_BINARIA.h
+++ Arvores/Binaria/ARV_BINARIA.h
@@ -2,10 +2,21 @@
 #define ARV_BINARIA_H
 
 typedef struct ARV_BINARIA{
-    char palavra_ingles[25];
+    char palavra_ingles[100];
     int unidade; 
     struct ARV_BINARIA *esquerda; 
     struct ARV_BINARIA *direita; 
 }ARV_BINARIA;
+
+ARV_BINARIA *cria_arvore_binaria(char *palavra_ingles, int unidade);
+void insere_arvore_binaria(ARV_BINARIA **arvore, char *palavra_ingles, int unidade);
+void libera_arvore_binaria(ARV_BINARIA **arvore);
+void mostrar_arvore_binaria(ARV_BINARIA *arvore,int unidade);
+
+int remover_no(ARV_BINARIA **arvore, char *palavra_ingles);
+int eh_folha_binaria(ARV_BINARIA *no);
+int tem_apenas_um_filho(ARV_BINARIA *no);
+int tem_dois_filhos(ARV_BINARIA *no);
+ARV_BINARIA *buscar_palavra(ARV_BINARIA *arvore, char *palavra_ingles);
 
 #endif
