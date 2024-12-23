5b629a0b3235e137fbc266a2f5f3ded1dcde4574
EmillySatiro <sousasatiroclaraemilly@gmail.com>
2024-12-23 16:44:58 -0300

    att

--- src/Arvores/Arvore_2_3/scripts/compilar_e_roldar_1.sh
+++ src/Arvores/Arvore_2_3/scripts/compilar_e_roldar_1.sh
@@ -7,19 +7,10 @@ BASE_DIR="/home/Trabalho-2-ed-II"
 EXECUTAVEL="programa_quest_1"
 
 # Arquivos necessários
-<<<<<<< HEAD:scripts/compilar_e_rodar.sh
-MAIN_FILE="$BASE_DIR/main.c"
-BINARIA_FILE="$BASE_DIR/Arvores/Binaria/ARV_BINARIA.c"
-ARVORE_2_3_FILE="$BASE_DIR/Arvores/Arvore_2_3/ARV2_3.c"
-RUBRO_NEGRA_FILE="$BASE_DIR/Arvores/Rubro_negra/Rubro_negra.c"
-EXTRAIR="$BASE_DIR/Arvores/Arvore_2_3/Arquivo/Extrair.c"
-# EXTRAIR_RUBRO="$BASE_DIR/Arvores/Rubro_negra/Arquivo/extrair_rubro.c"
-=======
 MAIN_FILE="$BASE_DIR/src/Arvores/Arvore_2_3/main_quest_2.c"
 BINARIA_FILE="$BASE_DIR/src/Arvores/Binaria/ARV_BINARIA.c"
 RUBRO_NEGRA_FILE="$BASE_DIR/src/Arvores/Arvore_2_3/ARV2_3.c"
 EXTRAIR_RUBRO="$BASE_DIR/src/Arvores/Arvore_2_3/Arquivo/Extrair.c"
->>>>>>> 29df7465d044d311d0ef31f934fe1d1facb1edef:src/Arvores/Arvore_2_3/scripts/compilar_e_roldar_1.sh
 
 # Verificar se os arquivos existem
 if [ ! -f "$MAIN_FILE" ]; then
@@ -40,27 +31,8 @@ fi
 if [ ! -f "$EXTRAIR_RUBRO" ]; then
     echo "Erro: $EXTRAIR_RUBRO não encontrado!"
     exit 1
-<<<<<<< HEAD:scripts/compilar_e_rodar.sh
-fi
-# if [ ! -f "$EXTRAIR_RUBRO" ]; then
-#     echo "Erro: $EXTRAIR_RUBRO não encontrado!"
-#     exit 1
-# fi  
-
-# Compilar o programa
-echo "Compilando o programa..."
-gcc -o "$EXECUTAVEL" \
--I"$BASE_DIR" \
-"$MAIN_FILE" \
-"$BINARIA_FILE" \
-"$ARVORE_2_3_FILE" \
-"$RUBRO_NEGRA_FILE" \
-"$EXTRAIR" 
-# "$EXTRAIR_RUBRO"
-=======
 fi  
 
-# Compilar o programa
 echo "Compilando o programa..."
 gcc -o "$BASE_DIR/$EXECUTAVEL" \
     -I"$BASE_DIR" \
@@ -69,7 +41,6 @@ gcc -o "$BASE_DIR/$EXECUTAVEL" \
     "$RUBRO_NEGRA_FILE" \
     "$EXTRAIR_RUBRO" \
     -Wall -Wextra -Werror -Wpedantic
->>>>>>> 29df7465d044d311d0ef31f934fe1d1facb1edef:src/Arvores/Arvore_2_3/scripts/compilar_e_roldar_1.sh
 
 # Verificar se a compilação foi bem-sucedida
 if [ $? -eq 0 ]; then
