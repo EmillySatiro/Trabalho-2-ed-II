7263202312b72042b554b565048fdbce0a136a0f
EmillySatiro <sousasatiroclaraemilly@gmail.com>
2024-12-24 17:32:41 -0300

    att structs

--- Questao3/src/ARVORE2_3.h
+++ Questao3/src/ARVORE2_3.h
@@ -1,24 +1,25 @@
-
 #ifndef ARVORE2_3_H
 #define ARVORE2_3_H
 
-typedef struct Informacao {
-    char state; // 'L' para livre, 'O' para ocupado
-    int block_inicio; // Bloco inicial
-    int block_fim;   // Bloco final
-} Informacao;
+#define LIVRE 0  
+#define OCUPADO 1 
+typedef struct Informacao_memoria{
+    int state; // '0' para livre, '1' para ocupado
+    int block_inicio; 
+    int block_fim;   
+} Informacao_memoria;
 
 typedef struct ARVORE2_3 {
-    Informacao info1;   // Primeira informação do nó
-    Informacao info2;   // Segunda informação do nó (se houver)
+    Informacao_memoria info1;   // Primeira informação do nó
+    Informacao_memoria info2;   // Segunda informação do nó (se houver)
     int quant_infos;    // Quantidade de informações no nó (1 ou 2)
-    struct ARV2_3 *esquerda; // Ponteiro para o filho da esquerda
-    struct ARV2_3 *centro;   // Ponteiro para o filho do centro
-    struct ARV2_3 *direita;  // Ponteiro para o filho da direita (se houver)
+    struct ARVORE2_3 *esquerda; // Ponteiro para o filho da esquerda
+    struct ARVORE2_3 *centro;   // Ponteiro para o filho do centro
+    struct ARVORE2_3 *direita;  // Ponteiro para o filho da direita (se houver)
 } ARVORE2_3;
 
 
-ARVORE2_3 *criarNo(Informacao info);
+ARVORE2_3 *criarNo(Informacao_memoria info);
 void cadastrarNos(ARVORE2_3** raiz, unsigned long ultimo_endereco);
 
 
